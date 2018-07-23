7a5a7077500f1ccc8a293ded7c635c5e69c3a8f3
Nandan Joshi <njoshi@redhat.com>
2018-07-23 09:10:45 -0700

    modifications to address space creating scripts for 0.21

--- 01_connect_loadaddr/create_address_space.sh
+++ 01_connect_loadaddr/create_address_space.sh
@@ -2,4 +2,4 @@
 DEFINITION=$1
 TOKEN=`oc whoami -t`
 
-curl -X POST -T $DEFINITION -k -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" https://$(oc get route restapi -o jsonpath='{.spec.host}')/apis/enmasse.io/v1/addressspaces
+curl -X POST -T $DEFINITION -k -H "Content-Type: application/json" -H "Authorization: Bearer $TOKEN" https://$(oc get route restapi -o jsonpath='{.spec.host}')/apis/enmasse.io/v1alpha1/namespaces/std-mt/addressspaces
